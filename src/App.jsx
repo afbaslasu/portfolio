56fe328127a7e05c986e452ebb712c08aa6822b7
afbaslasu <afbaslasu@gmail.com>
2025-06-04 00:42:13 +0200

    feat: portfolio project structure layout setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Welcome to smileLink</h1>
+      <h1 class="text-5xl font-light underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App
